b7c810e29f92df18a846d7800cfb928c4afd71e2
josue-acosta <hannibalsway001@gmail.com>
2020-02-16 10:44:58 -0600

    added JSON functionality

--- app/routes_article_02.py
+++ app/routes_article_02.py
@@ -1,21 +1,20 @@
 from flask import render_template
 from app import app
-import json, os
+import json
 
+with app.open_resource("templates/article-02/data.json", "r" ) as data_file:
+        data = json.load(data_file)
 
 @app.route('/article-02/welcome')
-def article_02_welcome():
-    with app.open_resource("templates/article-02/data.json", "r" ) as data_file:
-        data = json.load(data_file)
-    
+def article_02_welcome():    
     return render_template("article-02/welcome.html", **data)
 
 
-@app.route('/article-02/chapter-02')
-def article_02_chapter_01():
-    return render_template("article-02/chapter-01.html")
+@app.route('/article-02/chapter-01')
+def article_02_chapter_01():    
+    return render_template("article-02/chapter-01.html", **data)
 
 
 @app.route('/article-02/resource')
 def article_02_resource():
-    return render_template("article-02/resource.html")
+    return render_template("article-02/resource.html", **data)

b5824c0252f18f7f983b18a685420cae1068d447
josue-acosta <hannibalsway001@gmail.com>
2020-02-22 11:38:43 -0600

    finished second rough draft

--- app/routes_article_02.py
+++ app/routes_article_02.py
@@ -6,12 +6,12 @@ with app.open_resource("templates/article-02/data.json", "r" ) as data_file:
         data = json.load(data_file)
 
 @app.route('/article-02/welcome')
-def article_02_welcome():    
+def article_02_welcome():
     return render_template("article-02/welcome.html", **data)
 
 
 @app.route('/article-02/chapter-01')
-def article_02_chapter_01():    
+def article_02_chapter_01():
     return render_template("article-02/chapter-01.html", **data)
 
 
